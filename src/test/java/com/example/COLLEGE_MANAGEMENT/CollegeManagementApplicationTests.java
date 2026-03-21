cda243b0f017a32d55befa32ffeb2e412a992248
harsh <harshk2102@gmail.com>
2026-03-21 21:43:59 +0530

    Initial commit

---
+++ src/test/java/com/example/COLLEGE_MANAGEMENT/CollegeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.COLLEGE_MANAGEMENT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
